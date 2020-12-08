0248a7e69301fed39eb722a524247cd3c92e170c
Zdenek Veleba <zveleba@redhat.com>
2020-12-09 00:55:11 +0100

    In WebUI render DisplayStatus as Markdown and cache CRC data to improve performance

--- libpipeline/webui/static/refresh.js
+++ libpipeline/webui/static/refresh.js
@@ -19,14 +19,17 @@ function change_refresh_rate() {
 function refreshData() {
     $.getJSON(pipeline_data_url, function(data) {
         $.each(data['caseRuns'], function(index, caserun) {
-            $(".crc-"+caserun.id+" .crc_name").text(caserun.name);
-            $(".crc-"+caserun.id+" .crc_configuration").html(caserun.configuration);
-            $(".crc-"+caserun.id+" .crc_runningfor").text(caserun.running_for);
-            $(".crc-"+caserun.id+" .crc_displaystatus").html(caserun.display_status);
-            $(".crc-"+caserun.id+" .crc_workflow").text(caserun.workflow);
-            $(".crc-"+caserun.id+" .crc_result").text(caserun.result);
-            $(".crc-"+caserun.id+" .crc_state").text(caserun.state);
-            $(".crc-"+caserun.id+" .crc_cancel").prop('disabled', !caserun.active);
+            if (JSON.stringify($(".crc-"+caserun.id).first().data("cache")) != JSON.stringify(caserun)) {
+                $(".crc-"+caserun.id).first().data("cache", caserun)
+                $(".crc-"+caserun.id+" .crc_name").text(caserun.name);
+                $(".crc-"+caserun.id+" .crc_configuration").html(caserun.configuration);
+                $(".crc-"+caserun.id+" .crc_runningfor").text(caserun.running_for);
+                $(".crc-"+caserun.id+" .crc_displaystatus").html(marked(caserun.display_status));
+                $(".crc-"+caserun.id+" .crc_workflow").text(caserun.workflow);
+                $(".crc-"+caserun.id+" .crc_result").text(caserun.result);
+                $(".crc-"+caserun.id+" .crc_state").text(caserun.state);
+                $(".crc-"+caserun.id+" .crc_cancel").prop('disabled', !caserun.active);
+            };
         });
     });
 }
