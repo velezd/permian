0378f75aae11a47337176bda5d73d4fb9aebf17c
Zdenek Veleba <zveleba@redhat.com>
2020-12-14 14:23:06 +0100

    Improve performace of WebUI data cache

--- libpipeline/webui/static/refresh.js
+++ libpipeline/webui/static/refresh.js
@@ -32,17 +32,16 @@ function configuration_to_html(configs) {
 function refreshData() {
     $.getJSON(pipeline_data_url, function(data) {
         $.each(data, function(index, caserun) {
-            if (JSON.stringify($(".crc-"+caserun.id).first().data("cache")) != JSON.stringify(caserun)) {
-                $(".crc-"+caserun.id).first().data("cache", caserun)
-                $(".crc-"+caserun.id+" .crc_name").text(caserun.name);
-                $(".crc-"+caserun.id+" .crc_configuration").html(configuration_to_html(caserun.configuration));
-                $(".crc-"+caserun.id+" .crc_runningfor").text(caserun.running_for);
-                $(".crc-"+caserun.id+" .crc_displaystatus").html(marked(caserun.display_status));
-                $(".crc-"+caserun.id+" .crc_workflow").text(caserun.workflow);
-                $(".crc-"+caserun.id+" .crc_result").text(caserun.result);
-                $(".crc-"+caserun.id+" .crc_state").text(caserun.state);
-                $(".crc-"+caserun.id+" .crc_cancel").prop('disabled', !caserun.active);
-            };
+            if ($(".crc-"+caserun.id).first().data("cache") == JSON.stringify(caserun)) { return }; // Continue to next CRC if the cache matches
+            $(".crc-"+caserun.id).first().data("cache", JSON.stringify(caserun)); // Update cache
+            $(".crc-"+caserun.id+" .crc_name").text(caserun.name);
+            $(".crc-"+caserun.id+" .crc_configuration").html(configuration_to_html(caserun.configuration));
+            $(".crc-"+caserun.id+" .crc_runningfor").text(caserun.running_for);
+            $(".crc-"+caserun.id+" .crc_displaystatus").html(marked(caserun.display_status));
+            $(".crc-"+caserun.id+" .crc_workflow").text(caserun.workflow);
+            $(".crc-"+caserun.id+" .crc_result").text(caserun.result);
+            $(".crc-"+caserun.id+" .crc_state").text(caserun.state);
+            $(".crc-"+caserun.id+" .crc_cancel").prop('disabled', !caserun.active);
         });
     });
 }
