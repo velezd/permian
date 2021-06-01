2c96dd173dce676db94dc5c88ad27d5ef13ba5a8
Pavel Holica <pholica@redhat.com>
2021-06-01 10:23:02 +0200

    [RTT-2847] Support for brew events and convertion to compose event structure
    Note this plugin is currently capable to handle only internal Red Hat Brew
    instance.

---
+++ libpipeline/plugins/koji/__init__.py
@@ -0,0 +1,117 @@
+import os
+import re
+import json
+import xmlrpc.client
+import productmd
+import requests
+
+from libpipeline.plugins import api
+from libpipeline.events.base import Event
+from libpipeline.events.structures.builtin import ProductStructure
+from libpipeline.cli.parser import bool_argument, ToPayload, AppendToPayload
+
+from libpipeline.plugins.compose import ComposeStructure
+
+TAG_REGEXPS = (
+    # product-1.2.3-state
+    re.compile('(?P<product>[^-]+)-(?P<major>[0-9]+).(?P<minor>[0-9]+).(?P<qr>[0-9]+)-(?P<state>[^-]+)'),
+)
+
+def parse_koji_tag(tag):
+    for tag_regexp in TAG_REGEXPS:
+        mo = tag_regexp.match(tag)
+        if mo is None:
+            continue
+        return {
+            'product' : mo.group('product'),
+            'major' : mo.group('major'),
+            'minor' : mo.group('minor'),
+        }
+    return None
+
+@api.events.register_structure('koji_build')
+class KojiBuild():
+    def __init__(self, hub_url, nvr, build_id=None, task_id=None, package_name=None, tags=None, new_tag=None, composes_baseurl=None):
+        self._info = None
+        self.hub_url = hub_url
+        self.nvr = nvr
+        self.build_id = None # have it set to None until it's discovered
+        self.build_id = build_id or self.info['build_id']
+        self._tags = tags
+        self._new_tag = new_tag
+        self.task_id = task_id or self.info['task_id']
+        self.package_name = package_name or self.info['package_name']
+        self.composes_baseurl = composes_baseurl
+
+    @property
+    def info(self):
+        if self._info is not None:
+            return self._info
+        koji = xmlrpc.client.ServerProxy(self.hub_url)
+        self._info = koji.getBuild(self.build_id or self.nvr, True) # use build_id if specified
+        return self._info
+
+    @property
+    def tags(self):
+        if self._tags is not None:
+            return self._tags
+        koji = xmlrpc.client.ServerProxy(self.hub_url, allow_none=True)
+        self._tags = tuple(
+            tag['name'] for tag in
+            koji.listTags(self.build_id, None, False)
+        )
+        return self._tags
+
+    @property
+    def new_tag(self):
+        if self._new_tag is not None:
+            return self._new_tag
+        self._new_tag = self.tags[0]
+        return self._new_tag
+
+    def to_compose(self):
+        if self.composes_baseurl is None:
+            return NotImplemented
+        entrypoint = f'{self.composes_baseurl}/{self.task_id}-{self.package_name}'
+        entrypoint_dir = os.path.dirname(entrypoint)
+        compose_relpath = requests.get(entrypoint).text.strip()
+        compose_path = f'{entrypoint_dir}/{compose_relpath}'
+        compose_id = productmd.compose.Compose(compose_path).info.compose.id
+        return ComposeStructure(compose_id, location=compose_path)
+
+    def to_product(self):
+        parsed_tag = parse_koji_tag(self.new_tag)
+        return ProductStructure(
+            parsed_tag['product'],
+            parsed_tag['major'],
+            parsed_tag['minor'],
+        )
+
+@api.events.register('koji')
+class KojiEvent(Event):
+    def __init__(self, type, koji_build, **kwargs):
+        super().__init__(type, koji_build=koji_build, **kwargs)
+
+    def __str__(self):
+        return f"{self.koji_build.package_name} {self.koji_build.nvr} {self.koji_build.new_tag}"
+
+@api.cli.register_command_parser('koji_build_tag')
+def koji_build_tag_command(base_parser, args):
+    parser = base_parser
+    parser.add_argument('hub_url', action=ToPayload,
+                        help='URL of koji hub where the build was tagged')
+    parser.add_argument('nvr', action=ToPayload,
+                        help='Koji build nvr like abc-1.2-3.fc4')
+    # TODO: fix ToPayload to work with new-tag
+    parser.add_argument('new_tag', action=ToPayload,
+                        help='New tag that was added to the build')
+    parser.add_argument('--event-type', default='koji.build.tag')
+    parser.add_argument('--build-id', type=int, action=ToPayload,
+                        help='Override build id')
+    parser.add_argument('--task-id', type=int, action=ToPayload,
+                        help='Override task id')
+    parser.add_argument('--package-name', action=ToPayload,
+                        help='Override package name')
+    options = parser.parse_args(args)
+
+    return options, json.dumps({'type': options.event_type, 'koji_build': options.payload})
---
+++ libpipeline/plugins/koji/test.py
@@ -0,0 +1,125 @@
+import unittest
+import re
+from unittest.mock import patch, create_autospec
+import productmd
+
+from libpipeline.events.factory import EventFactory
+from libpipeline.cli.factory import CliFactory
+
+from . import KojiEvent, KojiBuild
+
+@patch('xmlrpc.client.ServerProxy')
+class TestKojiEvent(unittest.TestCase):
+    hub_url = 'http://koji.example.com/path/to/hub'
+    nvr = 'foo-1.2-3.dt4'
+    tag = 'bar'
+
+    def test_minimal(self, koji_proxy_class):
+        event = EventFactory.make(
+            CliFactory.parse('koji_build_tag', [self.hub_url, self.nvr, self.tag])[1]
+        )
+        self.assertIsInstance(event, KojiEvent)
+        self.assertIsInstance(event.koji_build, KojiBuild)
+        self.assertEqual(event.koji_build.nvr, self.nvr)
+        self.assertEqual(event.koji_build.new_tag, self.tag)
+        self.assertEqual(event.type, 'koji.build.tag')
+
+    def test_all(self, koji_proxy_class):
+        event_type = 'koji.hello'
+        build_id = 67890
+        task_id = 12345
+        package_name = 'stress-o-meter'
+        command_args = [
+            self.hub_url,
+            self.nvr,
+            self.tag,
+            '--event-type', event_type,
+            '--build-id', str(build_id),
+            '--task-id', str(task_id),
+            '--package-name', package_name,
+        ]
+        event = EventFactory.make(
+            CliFactory.parse('koji_build_tag', command_args)[1]
+        )
+        self.assertIsInstance(event, KojiEvent)
+        self.assertIsInstance(event.koji_build, KojiBuild)
+        self.assertEqual(event.koji_build.nvr, self.nvr)
+        self.assertEqual(event.koji_build.new_tag, self.tag)
+        self.assertEqual(event.type, event_type)
+        self.assertEqual(event.koji_build.build_id, build_id)
+        self.assertEqual(event.koji_build.task_id, task_id)
+        self.assertEqual(event.koji_build.package_name, package_name)
+        self.assertEqual(str(event), f'stress-o-meter {self.nvr} {self.tag}')
+
+@patch('xmlrpc.client.ServerProxy')
+class TestKojiEventStructure(unittest.TestCase):
+    hub_url = 'http://koji.example.com/path/to/hub'
+    nvr = 'foo-1.2-3.dt4'
+    build_id = 1001
+    task_id = 1337
+    package_name = 'surprise!'
+    new_tag = 'acme-1.2.3-poof'
+    composes_baseurl = 'http://example.com/composes/foo'
+
+    def test_minimal(self, koji_proxy_class):
+        koji_build = KojiBuild(self.hub_url, self.nvr)
+        self.assertEqual(koji_build.hub_url, self.hub_url)
+        self.assertEqual(koji_build.nvr, self.nvr)
+
+    def test_discovery(self, koji_proxy_class):
+        koji_proxy_class.return_value.getBuild.return_value = {
+            'build_id' : self.build_id,
+            'task_id' : self.task_id,
+            'package_name' : self.package_name,
+        }
+        koji_proxy_class.return_value.listTags.return_value = (
+            {'name': self.new_tag},
+            {'name': 'foo'},
+        )
+        koji_build = KojiBuild(self.hub_url, self.nvr)
+        self.assertEqual(koji_build.hub_url, self.hub_url)
+        self.assertEqual(koji_build.nvr, self.nvr)
+        self.assertEqual(koji_build.build_id, self.build_id)
+        self.assertEqual(koji_build.task_id, self.task_id)
+        self.assertEqual(koji_build.package_name, self.package_name)
+        self.assertEqual(koji_build.new_tag, self.new_tag)
+        koji_proxy_class.assert_called_with(self.hub_url, allow_none=True)
+        koji_proxy_class.return_value.getBuild.assert_called_once_with(self.nvr, True)
+        koji_proxy_class.return_value.listTags.assert_called_once_with(self.build_id, None, False)
+
+    def test_all(self, koji_proxy_class):
+        koji_build = KojiBuild(
+            self.hub_url, self.nvr, build_id=self.build_id,
+            task_id=self.task_id, package_name=self.package_name,
+            new_tag=self.new_tag
+        )
+        self.assertEqual(koji_build.nvr, self.nvr)
+        self.assertEqual(koji_build.build_id, self.build_id)
+        self.assertEqual(koji_build.task_id, self.task_id)
+        self.assertEqual(koji_build.package_name, self.package_name)
+        self.assertEqual(koji_build.new_tag, self.new_tag)
+        koji_proxy_class.assert_not_called()
+        koji_proxy_class.return_value.getBuild.assert_not_called()
+
+    @patch('requests.get')
+    @patch('productmd.compose.Compose')
+    def test_convert_compose(self, Compose, requests_get, koji_proxy_class):
+        compose_id = 'FooBar-1.23-123456.t.98'
+        compose_relpath = '../some_compose_dir'
+        koji_proxy_class.return_value.getBuild.return_value = {
+            'build_id' : self.build_id,
+            'task_id' : self.task_id,
+            'package_name' : self.package_name,
+        }
+        koji_proxy_class.return_value.listTags.return_value = (
+            {'name': self.new_tag},
+        )
+        Compose.return_value.info.compose.id = compose_id
+        requests_get.return_value.text = compose_relpath
+        koji_build = KojiBuild(self.hub_url, self.nvr, composes_baseurl=self.composes_baseurl)
+        compose = koji_build.to_compose()
+        self.assertEqual(compose.id, compose_id)
+        self.assertEqual(
+            compose.location,
+            f'{self.composes_baseurl}/{compose_relpath}'
+        )
