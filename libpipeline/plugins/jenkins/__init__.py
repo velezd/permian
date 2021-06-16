5e36aeddfa5ad0c729415dd787dd985fdd2c2433
Zdenek Veleba <zveleba@redhat.com>
2021-06-16 08:48:53 +0200

    Add functions for getting build urls to jenkins plugin

--- libpipeline/plugins/jenkins/__init__.py
+++ libpipeline/plugins/jenkins/__init__.py
@@ -8,15 +8,39 @@ from ...webui.hooks import WebUI_started, static_WebUI_rendered
 
 LOGGER = logging.getLogger(__name__)
 
+
+def required_build_info(settings):
+    """ Checks if all settings required for producing build urls are set """
+    try:
+        return all([ settings.get('jenkins', setting) != '' for setting in ['url', 'job_name', 'build_num'] ])
+    except KeyError:
+        return False
+
+
+def required_jenkins_settings(settings):
+    """ Checks if all settings required for interacting with jenkins build are set """
+    try:
+        return all([ settings.get('jenkins', setting) != '' for setting in ['url', 'username', 'password', 'job_name', 'build_num'] ])
+    except KeyError:
+        return False
+
+
+def get_build_url(settings):
+    return f'{settings.get("jenkins", "url")}/job/{settings.get("jenkins", "job_name")}/{settings.get("jenkins", "build_num")}'
+
+
+def get_build_log_url(settings):
+    return f'{get_build_url(settings)}/console'
+
+
 def set_webui_link_description(settings, event, URL, is_artifact=False):
-    build_num = settings.get('jenkins', 'build_num')
-    build_url = f'{settings.get("jenkins", "url")}/job/{settings.get("jenkins", "job_name")}/{build_num}'
+    build_url = get_build_url(settings)
     submit_url = f'{build_url}/configSubmit'
     artifact_baseurl = f'{build_url}/artifact'
     if is_artifact:
         URL = f'{artifact_baseurl}/{URL}'
     payload = {
-        'displayName': f'#{build_num}: {event}',
+        'displayName': f'#{settings.get("jenkins", "build_num")}: {event}',
         'description': f'<a href="{URL}">WebUI</a>',
     }
 
@@ -39,14 +63,6 @@ def set_jenkins_build_info(webUI):
         return
 
     set_webui_link_description(settings, webUI.pipeline.event, webUI.baseurl)
-
-
-def required_jenkins_settings(settings):
-    """ Checks if all settings required for interacting with jenkins build are set """
-    try:
-        return all([ settings.get('jenkins', setting) != '' for setting in ['url', 'username', 'password', 'job_name', 'build_num'] ])
-    except KeyError:
-        return False
 
 
 @threaded_callback_on(static_WebUI_rendered)
