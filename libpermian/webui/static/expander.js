78c5ee0291563afa12ab54c403fbbbf9f43e9c5a
Zdenek Veleba <zveleba@redhat.com>
2022-03-18 16:03:03 +0100

    Improve displaying of expanding log list with long log names
    New behavior can be seen in demo tc 'grub2 / grubenv_saved_entry'

--- libpermian/webui/static/expander.js
+++ libpermian/webui/static/expander.js
@@ -4,11 +4,13 @@ function expand_nextSibling(elem) {
         // Set maxHeight explicitly to none overriding the max-height for this
         // specific element.
         siblingStyle.maxHeight = 'none';
+        siblingStyle.overflowX = 'auto';
         elem.innerText = '▲'
     }
     else {
         // Remove the maxHeight override.
         siblingStyle.removeProperty('max-height');
+        siblingStyle.removeProperty('overflow-x');
         elem.innerText = '▼'
     }
 }
